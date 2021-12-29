28620235cb9b1c1660f6affea696dee908318253
shunsuke <dekcguzsbc@i.softbank.jp>
2021-12-29 16:01:02 +0900

    modify calender

--- 02.calendar/calender.rb
+++ 02.calendar/calender.rb
@@ -1,38 +1,39 @@
 require "date"
 require "optparse"
-require "paint"
 
 class Calender
-  def initialize(year: Date.today.year, month: Date.today.month)
+  def initialize(year, month)
     @year = year
     @month = month
   end  
+  
   def generate
-    firstday = Date.new(@year, @month, 1).wday
-    lastday = Date.new(@year, @month, -1).day
     top = Date.new(@year, @month).strftime("%m月 %Y")
-    day_of_week = [ "日", "月", "火", "水", "木", "金", "土" ]
-    
     puts top.center(20)
-    puts day_of_week.join(" ")
-    print "   " * firstday
+    
+    puts %w(日 月 火 水 木 金 土).join(" ")
+    
+    first_wday = Date.new(@year, @month, 1).wday
+    print "   " * first_wday
 
-    (1..lastday).each {|date|
+    last_day = Date.new(@year, @month, -1).day
+    (1..last_day).each do |date|
       if Date.new(@year, @month, date) == Date.today
-        print Paint[date, :white, :black].to_s.rjust(2) + " "
+        print "\e[37m\e[40m#{date.to_s.rjust(2)}\e[0m" + " "
       else 
         print date.to_s.rjust(2) + " "
       end
-      firstday = firstday + 1
-      if firstday %7 == 0
-        print "\n"
+      if Date.new(@year, @month, date).saturday?
+        print "\n" 
       end
-    }
+    end
+    print "\n"
   end
 end
+
 params = ARGV.getopts("y:", "m:")
 calender = Calender.new(
-  year: params["y"] &.to_i || Date.today.year, 
-  month: params["m"] &.to_i || Date.today.month
+params["y"] &.to_i || Date.today.year, 
+params["m"] &.to_i || Date.today.month
 )
 calender.generate
