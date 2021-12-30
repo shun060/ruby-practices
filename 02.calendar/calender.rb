5bd604cfa00f900ae96f77d3aac6da55e8d98f76
shunsuke <dekcguzsbc@i.softbank.jp>
2021-12-31 03:11:04 +0900

    assign day.to_s.rjust(2) to a variabl. Changed the output method of the day of the week. Add indentation on lines 40 and 41

--- 02.calendar/calender.rb
+++ 02.calendar/calender.rb
@@ -11,18 +11,21 @@ class Calender
     top = Date.new(@year, @month).strftime("%m月 %Y")
     puts top.center(20)
     
-    puts %w(日 月 火 水 木 金 土).join(" ")
+    puts "日 月 火 水 木 金 土"
     
     first_wday = Date.new(@year, @month, 1).wday
     print "   " * first_wday
 
     last_day = Date.new(@year, @month, -1).day
-    (1..last_day).each do |date|
-      current_date = Date.new(@year, @month, date)
-      if  current_date == Date.today
-        print "\e[37m\e[40m#{date.to_s.rjust(2)}\e[0m" + " "
+    
+    (1..last_day).each do |day|
+      current_date = Date.new(@year, @month, day)
+      align_day = day.to_s.rjust(2)
+
+      if current_date == Date.today
+        print "\e[37m\e[40m#{align_day}\e[0m" + " "
       else 
-        print date.to_s.rjust(2) + " "
+        print align_day + " "
       end
       if current_date.saturday?
         print "\n" 
@@ -34,7 +37,7 @@ end
 
 params = ARGV.getopts("y:", "m:")
 calender = Calender.new(
-params["y"] &.to_i || Date.today.year, 
-params["m"] &.to_i || Date.today.month
+  params["y"] &.to_i || Date.today.year, 
+  params["m"] &.to_i || Date.today.month
 )
 calender.generate
